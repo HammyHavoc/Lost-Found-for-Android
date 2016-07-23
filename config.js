471065f4f8cc1705ce1c0526d2d907b6457a94c4
Hammy Havoc <hammy@splitanatom.com>
2016-07-23 19:52:53 +0100

    Initial commit.

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'lost-found-android',
+		wp_ws_url : 'https://www.lostandfound.mu/wp-appkit-api/lost-found-android',
+		wp_url : 'https://www.lostandfound.mu',
+		theme : 'q-android',
+		version : '1.0',
+		app_title : 'Lost & Found',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : '|MyM]VLWWO{_C7m;Xa#!h-,tYl|FxLDFOI#pdX78zML&f7BCK&G-xg8Plg.0u*ah',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
